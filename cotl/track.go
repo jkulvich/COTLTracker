65d66840dda6ed7bcb90bc82cc045cba7d4c97f7
Yuri Kulagin <jkulvichi@gmail.com>
2026-01-23 21:57:17 +0300

    NO-TASK: added Sky Music Sheet Maker format support (crunchy), added Payphone track
    Signed-off-by: Yuri Kulagin <jkulvichi@gmail.com>

--- cotl/track.go
+++ cotl/track.go
@@ -1,9 +1,11 @@
 package cotl
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"player/cotl/block"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,6 +19,104 @@ type Track struct {
 
 // NewTrack - Парсит нотное представление и представляет его в виде трека
 func NewTrack(stave string) (*Track, error) {
+	if json.Valid([]byte(stave)) {
+		if track, err := NewTrackFromSkyMusicSheet(stave); err == nil {
+			return track, nil
+		}
+	}
+
+	return NewTrackFromCOTLTrackerFormat(stave)
+}
+
+// NewTrackFromSkyMusicSheet - Sky Music Sheet Maker format.
+// This is a very-very crunchy parser, but it is quick hack to work with initially unsupported format :)
+func NewTrackFromSkyMusicSheet(stave string) (*Track, error) {
+	var parties []struct {
+		Name         string `json:"name"`
+		Author       string `json:"author"`
+		TanscribedBy string `json:"transcribedBy"`
+		IsComposed   bool   `json:"isComposed"`
+		BpM          int    `json:"bpm"`
+		BitsPerPage  int    `json:"bitsPerPage"`
+		PitchLevel   int    `json:"pitchLevel"`
+		IsEncrypted  bool   `json:"isEncrypted"`
+		SongNotes    []struct {
+			Time int    `json:"time"`
+			Key  string `json:"key"`
+		} `json:"songNotes"`
+	}
+
+	if err := json.Unmarshal([]byte(stave), &parties); err != nil {
+		return nil, err
+	}
+
+	part := parties[0]
+
+	// sort notes to be consistent
+	sort.Slice(part.SongNotes, func(i, j int) bool {
+		return part.SongNotes[i].Time < part.SongNotes[j].Time
+	})
+
+	track := Track{
+		blocks: make(block.Blocks, 0),
+		shift:  0,
+		timing: 200,
+	}
+
+	// iterate over notes and create blocks
+	previousTime := 0
+	posCounter := 0
+	for iNote, note := range part.SongNotes {
+		if previousTime < note.Time {
+			delayTime := note.Time - previousTime
+			delayTime = int(float32(delayTime) * 0.9) //< delay compensation from ADB input
+			delay, err := block.NewDelay(strconv.Itoa(delayTime), 0)
+			if err != nil {
+				return nil, err
+			}
+			// skip initial delay
+			if iNote > 0 {
+				posCounter++
+				delayBlocks := delay.GetBlocks()
+				delayBlocks[0].LineNum = posCounter
+				track.blocks = append(track.blocks, delayBlocks...)
+			}
+			previousTime = note.Time
+		}
+		mapping := map[string]string{
+			"1Key0": "C0",
+			"1Key1": "D0",
+			"1Key2": "E0",
+			"1Key3": "F0",
+			"1Key4": "G0",
+
+			"1Key5": "A0",
+			"1Key6": "B0",
+			"1Key7": "C1",
+			"1Key8": "D1",
+			"1Key9": "E1",
+
+			"1Key10": "F1",
+			"1Key11": "G1",
+			"1Key12": "A1",
+			"1Key13": "B1",
+			"1Key14": "C2",
+		}
+		note, err := block.NewNote(mapping[note.Key])
+		if err != nil {
+			return nil, err
+		}
+		posCounter++
+		noteBlocks := note.GetBlocks()
+		noteBlocks[0].LineNum = posCounter
+		track.blocks = append(track.blocks, noteBlocks...)
+	}
+
+	return &track, nil
+}
+
+// NewTrackFromCOTLTrackerFormat - COTLTracker format
+func NewTrackFromCOTLTrackerFormat(stave string) (*Track, error) {
 	track := Track{
 		blocks: make(block.Blocks, 0),
 		shift:  0,
